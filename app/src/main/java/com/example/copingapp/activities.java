a41a593868c4756aa6b63ff06fec390035ba6ce1
aaprilfools <aprilrl2@illinois.edu>
2019-12-07 19:24:37 -0600

    activities

--- app/src/main/java/com/example/copingapp/activities.java
+++ app/src/main/java/com/example/copingapp/activities.java
@@ -3,6 +3,18 @@ package com.example.copingapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
 
 public class activities extends AppCompatActivity {
 
@@ -10,5 +22,41 @@ public class activities extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_activities);
+        Button activitiesButton = findViewById(R.id.activitiesButton);
+        activitiesButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getActivity();
+            }
+        });
+    }
+
+    public void getActivity() {
+        final TextView textView = findViewById(R.id.activityText);
+
+        // Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url ="http://www.boredapi.com/api/activity/";
+
+        // Request a string response from the provided URL.
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            textView.setText(response.get("activity").toString());
+                        } catch(Exception e) {
+                            textView.setText("Uh oh!");
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                textView.setText("That didn't work!");
+            }
+        });
+
+        // Add the request to the RequestQueue.
+        queue.add(jsonObjectRequest);
     }
 }
