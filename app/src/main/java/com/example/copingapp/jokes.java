99577e8e9d2a991d6015b038cb42cfd90c5cfff8
aaprilfools <aprilrl2@illinois.edu>
2019-12-07 19:38:34 -0600

    jokes

--- app/src/main/java/com/example/copingapp/jokes.java
+++ app/src/main/java/com/example/copingapp/jokes.java
@@ -3,6 +3,18 @@ package com.example.copingapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
 
 public class jokes extends AppCompatActivity {
 
@@ -10,5 +22,42 @@ public class jokes extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_jokes);
+        Button jokeButton = findViewById(R.id.jokeButton);
+        jokeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getJoke();
+            }
+        });
+    }
+
+    public void getJoke() {
+        final TextView textView = findViewById(R.id.setup);
+        final TextView punchline = findViewById(R.id.punchline);
+        // Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url ="https://official-joke-api.appspot.com/random_joke";
+
+        // Request a string response from the provided URL.
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            textView.setText(response.get("setup").toString());
+                            punchline.setText(response.get("punchline").toString());
+                        } catch(Exception e) {
+                            textView.setText("Uh oh!");
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                textView.setText("That didn't work!");
+            }
+        });
+
+        // Add the request to the RequestQueue.
+        queue.add(jsonObjectRequest);
     }
 }
